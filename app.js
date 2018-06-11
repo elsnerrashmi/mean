4bc4bd4ea6eba77d11f1f8325e8d71d39d3a29ee
rashmidholakia <rashmidholakia@yahoo.com>
2018-06-11 10:00:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let appRoutes = require('./routes/app');
+let messageRoutes = require('./routes/messages');
+let authRoutes = require('./routes/auth');
+
+let app = express();
+mongoose.connect('mongodb://localhost:27017/mean');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/message', messageRoutes);
+app.use('/auth', authRoutes);
+app.use('/', appRoutes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    return res.render('index');
+});
+
+
+module.exports = app;
