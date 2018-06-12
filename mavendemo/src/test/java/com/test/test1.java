50602bd9e6276b5d3420389bd0d1dacab30275ce
elsnerrashmi <39724525+elsnerrashmi@users.noreply.github.com>
2018-06-12 18:53:09 +0530

    script changes

--- mavendemo/src/test/java/com/test/test1.java
+++ mavendemo/src/test/java/com/test/test1.java
@@ -3,13 +3,15 @@ package com.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 
 public class test1 {
 
 	@Test
 	public void test() {
-		
-		System.out.println("Hello");		
+		WebDriver driver = null;
+		System.out.println("Hello");
+		driver.get("https://www.google.co.in/");
 	}
 
 }
