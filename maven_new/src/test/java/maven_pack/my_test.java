3373ad048f337cddd5d78f26204fe3988857eb62
elsnerrashmi <39724525+elsnerrashmi@users.noreply.github.com>
2018-06-13 15:25:10 +0530

    run jar file

--- maven_new/src/test/java/maven_pack/my_test.java
+++ maven_new/src/test/java/maven_pack/my_test.java
@@ -1,9 +1,38 @@
 package maven_pack;
 
-public class my_test {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-	public static void main(String[] args) {
+public class my_test {
+	
+	static WebDriver driver;
+	public static void main(String[] args) throws InterruptedException {
+		
 		System.out.println("hellloo.......");
+		System.setProperty("webdriver.gecko.driver", "driver\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		 driver.get("https://test.corepractice.is/#/");
+		    driver.findElement(By.id("Password")).clear();
+		    driver.findElement(By.id("Password")).sendKeys("ShahElsner2018");
+		    driver.findElement(By.id("Username")).clear();
+		    driver.findElement(By.id("Username")).sendKeys("harshal");
+		    driver.findElement(By.xpath("//button[@type='submit']")).click();
+		    Thread.sleep(5000);
+		    driver.findElement(By.xpath("//div[@id='navbar']/ul/li/a/span")).click();
+		    driver.findElement(By.xpath("(//a[contains(text(),'Appointment Book')])[2]")).click();
+		    Thread.sleep(5000);
+		    driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+		    driver.findElement(By.linkText("Default Practice")).click();
+		    Thread.sleep(5000);
+		    driver.findElement(By.xpath("//*[@data-time='09:00:00']")).click();
+		    driver.findElement(By.linkText("Select Patient")).click();
+		    driver.findElement(By.name("Firstname")).clear();
+		    driver.findElement(By.name("Firstname")).sendKeys("harshal");
+		    driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+		    driver.findElement(By.linkText("Mr Harshal Shah")).click();
+		    Thread.sleep(5000);
+		    driver.findElement(By.xpath("//button[@type='submit']")).click();
 	}
 
 }
