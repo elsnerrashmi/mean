4ba10e32e813b94a1e3199e6ea5f1a628b3bd5ee
elsnerrashmi <39724525+elsnerrashmi@users.noreply.github.com>
2018-06-13 15:43:46 +0530

    jar files

--- maven_new/src/test/java/maven_pack/my_test.java
+++ maven_new/src/test/java/maven_pack/my_test.java
@@ -1,7 +1,10 @@
 package maven_pack;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class my_test {
@@ -10,29 +13,32 @@ public class my_test {
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.out.println("hellloo.......");
-		System.setProperty("webdriver.gecko.driver", "driver\\geckodriver.exe");
-		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		 driver.get("https://test.corepractice.is/#/");
-		    driver.findElement(By.id("Password")).clear();
-		    driver.findElement(By.id("Password")).sendKeys("ShahElsner2018");
-		    driver.findElement(By.id("Username")).clear();
-		    driver.findElement(By.id("Username")).sendKeys("harshal");
-		    driver.findElement(By.xpath("//button[@type='submit']")).click();
-		    Thread.sleep(5000);
-		    driver.findElement(By.xpath("//div[@id='navbar']/ul/li/a/span")).click();
-		    driver.findElement(By.xpath("(//a[contains(text(),'Appointment Book')])[2]")).click();
-		    Thread.sleep(5000);
-		    driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
-		    driver.findElement(By.linkText("Default Practice")).click();
-		    Thread.sleep(5000);
-		    driver.findElement(By.xpath("//*[@data-time='09:00:00']")).click();
-		    driver.findElement(By.linkText("Select Patient")).click();
-		    driver.findElement(By.name("Firstname")).clear();
-		    driver.findElement(By.name("Firstname")).sendKeys("harshal");
-		    driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
-		    driver.findElement(By.linkText("Mr Harshal Shah")).click();
-		    Thread.sleep(5000);
-		    driver.findElement(By.xpath("//button[@type='submit']")).click();
+//		    driver.findElement(By.id("Password")).clear();
+//		    driver.findElement(By.id("Password")).sendKeys("ShahElsner2018");
+//		    driver.findElement(By.id("Username")).clear();
+//		    driver.findElement(By.id("Username")).sendKeys("harshal");
+//		    driver.findElement(By.xpath("//button[@type='submit']")).click();
+//		    Thread.sleep(5000);
+//		    driver.findElement(By.xpath("//div[@id='navbar']/ul/li/a/span")).click();
+//		    driver.findElement(By.xpath("(//a[contains(text(),'Appointment Book')])[2]")).click();
+//		    Thread.sleep(5000);
+//		    driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+//		    driver.findElement(By.linkText("Default Practice")).click();
+//		    Thread.sleep(5000);
+//		    driver.findElement(By.xpath("//*[@data-time='09:00:00']")).click();
+//		    driver.findElement(By.linkText("Select Patient")).click();
+//		    driver.findElement(By.name("Firstname")).clear();
+//		    driver.findElement(By.name("Firstname")).sendKeys("harshal");
+//		    driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+//		    driver.findElement(By.linkText("Mr Harshal Shah")).click();
+//		    Thread.sleep(5000);
+//		    driver.findElement(By.xpath("//button[@type='submit']")).click();
+		    driver.quit();
 	}
 
 }
