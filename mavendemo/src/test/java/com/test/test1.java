237acb6e49878ca1ac05599526af173fe6aaac5f
elsnerrashmi <39724525+elsnerrashmi@users.noreply.github.com>
2018-06-13 10:19:20 +0530

    Revert "script changes"
    This reverts commit 50602bd9e6276b5d3420389bd0d1dacab30275ce.

--- mavendemo/src/test/java/com/test/test1.java
+++ mavendemo/src/test/java/com/test/test1.java
@@ -3,15 +3,13 @@ package com.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
 
 public class test1 {
 
 	@Test
 	public void test() {
-		WebDriver driver = null;
-		System.out.println("Hello");
-		driver.get("https://www.google.co.in/");
+		
+		System.out.println("Hello");		
 	}
 
 }
