ef20e3c5e9237a791b887f872d32ee57082046ac
elsnerrashmi <39724525+elsnerrashmi@users.noreply.github.com>
2018-06-15 18:05:45 +0530

    added linux jar

--- TeamCityDemo/src/TeamCityDemo/TeamCityDemo.java
+++ TeamCityDemo/src/TeamCityDemo/TeamCityDemo.java
@@ -8,20 +8,23 @@ public class TeamCityDemo {
 	static WebDriver driver;
 	public static void main(String[] args) throws Exception {
 		System.out.println("Launching Browser");
-		System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		driver = new ChromeDriver();
 		System.out.println("Browser Launch");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://localhost:8292/auth/signin");
 		System.out.println("Open URL");
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("sagar@xhtmljunkies.com");
+		System.out.println("Login with : test@xhtmljunkies.com");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("test@xhtmljunkies.com");
 		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("1234567");
 		driver.findElement(By.xpath("//button[contains(.,'Submit')]")).click();
 		Thread.sleep(2000);
-		System.out.println("Login Validition Error: "+driver.findElement(By.xpath("//h5[@class='modal-title']")).getText());
+		System.out.println("Login Validition Error: "+driver.findElement(By.xpath("//p[contains(.,'Credentials do not match with our records.')]")).getText());
 		driver.findElement(By.xpath("//button[contains(.,'Close')]")).click();
 		driver.get("http://localhost:8292/auth/signin");
+		System.out.println("Login with : sagar@xhtmljunkies.com");
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("sagar@xhtmljunkies.com");
 		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123456");
 		driver.findElement(By.xpath("//button[contains(.,'Submit')]")).click();
